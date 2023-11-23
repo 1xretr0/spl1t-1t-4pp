96618aeca38dcef42989532677844930f51f6975
retr0 <sebasmorandez@hotmail.com>
2023-11-22 21:38:30 -0600

    Create user class.

---
+++ app/User.kt
@@ -0,0 +1,15 @@
+class User {
+    public lateinit var name : String
+    public lateinit var email : String
+    public lateinit var id : Int
+
+    constructor(id: Int) {
+        // TODO
+    }
+
+    constructor(name: String) {
+        // TODO
+    }
+
+    // TODO
+}
