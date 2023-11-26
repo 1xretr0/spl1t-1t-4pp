8dac4ccf3acc9967216b37a110cb4a0eb41065b8
retr0 <sebasmorandez@hotmail.com>
2023-11-25 18:48:48 -0600

    Rewrite Database function. Comment data inserts to test. Move function from login class to database class.

--- app/src/main/java/com/example/splitit/Database.kt
+++ app/src/main/java/com/example/splitit/Database.kt
@@ -1,45 +1,15 @@
 package com.example.splitit
 
-import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class Database(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
-    companion object {
-        private const val DATABASE_NAME = "db"
-
-        //Users Table
-        private const val NAME ="name"
-        private const val LASTNAME ="lastname"
-        private const val EMAIL ="lastname"
-        private const val PASSWORD ="lastname"
-
-        //Friends Table
-        private const val ID_FRIEND ="id_friend"  //PK
-        private const val ID_USER1 ="id_user1" //FK from users
-        private const val ID_USER2 ="id_user2" //FK from users
-        private const val STATUS ="status"
-
-        // Splits TABLE
-        private const val ID_SPLIT ="id_split" //PK
-        private const val ID_USER ="id_user" //FK from users
-        private const val TOTAL ="total" // Total Amount
-        private const val DATE ="date"
-
-        // TODO
-        // Split details TABLE
-        //private const val ID_DETAIL ="id_detail" //PK
-        //private const val ID_PAYMENT ="id_payment" FK from Payment
-        //private const val ID_USER ="id_user" //FK from users
-        private const val INDIVIDUAL_AMOUNT ="individual_amount"
-    }
-
-    override fun onCreate(db: SQLiteDatabase?) {
+class Database(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase) {
         // CREATE USERS TABLE
-        db?.execSQL("CREATE TABLE si_users(id integer primary key autoincrement, name varchar, lastname varchar, email varchar, password varchar )")
+        db.execSQL("CREATE TABLE si_users(id integer primary key autoincrement, name varchar, lastname varchar, email varchar, password varchar)")
 
-        val usersCV = ContentValues() // INSERT INTO USERS TABLE
+        /*val usersCV = ContentValues() // INSERT INTO USERS TABLE
         usersCV.put(NAME, "Mauricio")
         usersCV.put(LASTNAME, "Solano Nájera")
         usersCV.put(EMAIL, "mau.solano@gmail.com")
@@ -56,12 +26,12 @@ class Database(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
         usersCV.put(LASTNAME, "Moran Hernandez")
         usersCV.put(EMAIL, "sebas.moran@gmail.com")
         usersCV.put(PASSWORD, "everlong123")
-        db?.insert("si_users", NAME, usersCV)
+        db?.insert("si_users", NAME, usersCV)*/
 
         // CREATE FRIENDS TABLE
-        db?.execSQL("CREATE TABLE si_friends(id_friend integer primary key autoincrement, id_user1 int, id_user2 int, status varchar, FOREIGN KEY (id_user1) REFERENCES si_users(id), FOREIGN KEY (id_user2) REFERENCES si_users(id))")
+        db.execSQL("CREATE TABLE si_friends(id_friend integer primary key autoincrement, id_user1 int, id_user2 int, status varchar, FOREIGN KEY (id_user1) REFERENCES si_users(id), FOREIGN KEY (id_user2) REFERENCES si_users(id))")
 
-        val friendsCV = ContentValues() // INSERT INTO FRIENDS TABLE
+        /* val friendsCV = ContentValues() // INSERT INTO FRIENDS TABLE
         friendsCV.put(ID_USER1, 1)
         friendsCV.put(ID_USER2, 2)
         friendsCV.put(STATUS, "accepted") //Friends
@@ -75,12 +45,12 @@ class Database(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
         friendsCV.put(ID_USER1, 2)
         friendsCV.put(ID_USER2, 3)
         friendsCV.put(STATUS, "waiting") //Not friends yet
-        db?.insert("si_friends", ID_USER1, friendsCV)
+        db?.insert("si_friends", ID_USER1, friendsCV) */
         
         //CREATE PAYMENTS TABLE
-        db?.execSQL("create table si_splits(id_split integer primary key autoincrement, id_user int, total decimal, date date, FOREIGN KEY (id_user) REFERENCES si_users(id))")
+        db.execSQL("CREATE TABLE si_splits(id_split integer primary key autoincrement, id_user int, total decimal, date date, FOREIGN KEY (id_user) REFERENCES si_users(id))")
         
-        val splitsCV = ContentValues() //INSERT VALUES INTO PAYMENTS TABLE
+        /*val splitsCV = ContentValues() //INSERT VALUES INTO PAYMENTS TABLE
         splitsCV.put(ID_USER, 1)
         splitsCV.put(TOTAL, 102.00)
         splitsCV.put(DATE, "2023-11-16")
@@ -94,12 +64,12 @@ class Database(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
         splitsCV.put(ID_USER, 3)
         splitsCV.put(TOTAL, 600.00)
         splitsCV.put(DATE, "2023-11-18")
-        db?.insert("si_splits", ID_USER, splitsCV)
+        db?.insert("si_splits", ID_USER, splitsCV)*/
 
         //CREATE SPLIT DETAILS TABLE
-        db?.execSQL("create table si_split_details(id_detail integer primary key autoincrement, id_split int, id_user int, individual_amount decimal, FOREIGN KEY (id_split) REFERENCES si_splits(id_split), FOREIGN KEY (id_user) REFERENCES si_users(id))")
+        db.execSQL("CREATE TABLE si_split_details(id_detail integer primary key autoincrement, id_split int, id_user int, individual_amount decimal, FOREIGN KEY (id_split) REFERENCES si_splits(id_split), FOREIGN KEY (id_user) REFERENCES si_users(id))")
 
-        val splitDetailsCV = ContentValues() //INSERT VALUES INTO PAYMENT DETAILS TABLE
+        /*val splitDetailsCV = ContentValues() //INSERT VALUES INTO PAYMENT DETAILS TABLE
         //SPLIT FIRST PAYMENT
         splitDetailsCV.put(ID_SPLIT, 1)
         splitDetailsCV.put(ID_USER, 2)
@@ -141,10 +111,69 @@ class Database(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
         splitDetailsCV.put(ID_SPLIT, 3)
         splitDetailsCV.put(ID_USER, 3)
         splitDetailsCV.put(INDIVIDUAL_AMOUNT, 49.50)
-        db?.insert("si_split_details", ID_SPLIT, splitDetailsCV)
+        db?.insert("si_split_details", ID_SPLIT, splitDetailsCV)*/
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        android.util.Log.w("constants", "Actualizando la base de datos. Se destruirá la información anterior")
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        android.util.Log.w("constants", "Updating database. Data will be removed!")
+        db.execSQL("DROP TABLE IF EXISTS si_users")
+        db.execSQL("DROP TABLE IF EXISTS si_friends")
+        db.execSQL("DROP TABLE IF EXISTS si_splits")
+        db.execSQL("DROP TABLE IF EXISTS si_split_details")
+        onCreate(db)
+    }
+
+    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db, oldVersion, newVersion)
+    }
+
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "db"
+
+        //Users Table
+        const val NAME ="name"
+        const val LASTNAME ="lastname"
+        const val EMAIL ="lastname"
+        const val PASSWORD ="lastname"
+
+        //Friends Table
+        const val ID_FRIEND ="id_friend"  //PK
+        const val ID_USER1 ="id_user1" //FK from users
+        const val ID_USER2 ="id_user2" //FK from users
+        const val STATUS ="status"
+
+        // Splits TABLE
+        const val ID_SPLIT ="id_split" //PK
+        const val ID_USER ="id_user" //FK from users
+        const val TOTAL ="total" // Total Amount
+        const val DATE ="date"
+
+        // Split details TABLE
+        const val ID_DETAIL ="id_detail" //PK
+        const val ID_PAYMENT ="id_payment" // FK from Payment
+        const val INDIVIDUAL_AMOUNT ="individual_amount"
+    }
+
+    fun getUserFromDB(projection: Array<String>, selection: String, selectionArgs: Array<String>, order: String? = null) {
+        val db = this.readableDatabase
+        val cursor = db.query(
+            "si_users",
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            order
+        )
+
+        val itemIds = mutableListOf<Long>()
+        with(cursor) {
+            while (moveToNext()) {
+                val itemId = getLong(getColumnIndexOrThrow(ID_USER))
+                itemIds.add(itemId)
+            }
+        }
+        cursor.close()
     }
 }
--- app/src/main/java/com/example/splitit/LoginActivity.kt
+++ app/src/main/java/com/example/splitit/LoginActivity.kt
@@ -1,9 +1,9 @@
 package com.example.splitit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var emailEdt : EditText
@@ -18,15 +18,16 @@ class LoginActivity : AppCompatActivity() {
 
         val loginBtn = findViewById<Button>(R.id.login_form_btn)
         loginBtn.setOnClickListener {
-            getUserIdFromDB(
-                emailEdt.text.toString(),
-                passwdEdt.text.toString()
+            val dbHelper = Database(this)
+            val selection = "${Database.EMAIL} = ? AND ${Database.PASSWORD} = ?"
+
+            dbHelper.getUserFromDB(
+                arrayOf(Database.ID_USER),
+                selection,
+                arrayOf(emailEdt.text.toString(), passwdEdt.text.toString()),
+                null
             )
+            TODO("Work with db result")
         }
     }
-
-    private fun getUserIdFromDB(email: String, passwd: String) {
-
-    }
-
 }
